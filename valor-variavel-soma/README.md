a678f625859bdc02410d042b35a7c974a1745833
Daniel <danieljunior.08@hotmail.com>
2024-08-30 20:26:22 -0300

    Questão 3

---
+++ valor-variavel-soma/README.md
@@ -0,0 +1,5 @@
+## Valor da variável SOMA
+
+    // QUESTÃO 3
+
+        Este código é para descobrir o valor da variável soma.
---
+++ valor-variavel-soma/src/ValorVariavelSoma.java
@@ -0,0 +1,16 @@
+public class ValorVariavelSoma {
+    public static void main(String[] args) throws Exception {
+        
+        int indice = 12;
+        int soma = 0;
+        int k = 1;
+
+        while(k < indice){
+            k = k + 1;
+            soma = soma + k;
+        }
+
+        System.out.println(soma);
+
+    }
+}
